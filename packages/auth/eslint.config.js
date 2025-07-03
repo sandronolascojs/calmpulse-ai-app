4ac02ef5977b534aa1b4f1f9bf7aff14789d1528
Sandro Nolasco <sandronolasco1@gmail.com>
2025-07-03 03:01:44 -0300

    feat: implement better auth

---
+++ packages/auth/eslint.config.js
@@ -0,0 +1,3 @@
+const eslintConfig = require('@calmpulse-app/config/.eslintrc.base.js');
+
+module.exports = [...eslintConfig];
---
+++ packages/auth/src/config/env.config.ts
@@ -0,0 +1,9 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    ALLOWED_ORIGINS: z.string().default("http://localhost:3000"),
+  },
+  runtimeEnv: process.env,
+});
