3bc0bb95a6c4a8a0e2dbd2c7ee6fe08ee1384764
Sandro Nolasco <sandronolasco1@gmail.com>
2025-07-05 17:24:17 -0300

    fix: fix husky

--- .husky/_/pre-commit.sh
+++ .husky/_/pre-commit.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env sh
-. "$(dirname -- "$0")/_/husky.sh"
-+set -euo pipefail
-
-pnpm lint:fix          # fix whole repo first
-git add -u             # restage possible fixes
-pnpx lint-staged       # run staged-only checks
+pnpm lint:fix
+git add -u
+pnpx lint-staged
 pnpm typecheck
