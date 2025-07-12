ccd867e891b226d5cd4292b68e20eb9c94ed8243
Sandro Nolasco <sandronolasco1@gmail.com>
2025-07-11 21:18:08 -0300

    fix: fix ssr client and chart component

--- apps/frontend/src/app/(protected)/(dashboard)/layout.tsx
+++ apps/frontend/src/app/(protected)/(dashboard)/layout.tsx
@@ -1,10 +1,20 @@
-import { getUserWorkspace } from '@/lib/auth-helpers';
+import { QUERY_KEYS } from '@/constants/queryKeys.constants';
+import { createServerTsrClient } from '@/lib/tsr-client';
+import { QueryClient } from '@tanstack/react-query';
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const workspace = await getUserWorkspace();
+  const queryClient = new QueryClient();
+  const queryKey = QUERY_KEYS.WORKSPACE.GET_USER_WORKSPACE();
+  const cookie = (await cookies()).toString();
+  const serverTsrClient = await createServerTsrClient(cookie);
+  const tsrQueryClient = serverTsrClient.initQueryClient(queryClient);
+  const result = await tsrQueryClient.workspaceContract.getUserWorkspace.fetchQuery({
+    queryKey,
+  });
 
-  if (!workspace) {
+  if (!result.body.workspace) {
     redirect('/onboarding');
   }
 
